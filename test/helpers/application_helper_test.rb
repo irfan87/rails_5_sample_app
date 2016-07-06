223ef92fb29bd425a2fe946df0013e966a2f8800
irfan87 <nerve2009@yahoo.com>
2016-07-06 17:56:20 +0800

    finish layout and routes

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class ApplicationHelperTest < ActionView::TestCase
+	# test "full title helper" do
+	# 	assert_equal full_title, 'Sample App for Rails 5'
+	# 	assert_equal full_title("Help"), 'Sample App for Rails 5'
+	# end
+end
